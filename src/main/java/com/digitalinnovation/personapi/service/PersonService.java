6907d1e981d7e45c956988f6e9c5d59dc645dee9
rodyokoo <nihonrodrigo@hotmail.com>
2021-08-04 21:46:23 -0400

    cpf maks implementation

--- src/main/java/com/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/com/digitalinnovation/personapi/service/PersonService.java
@@ -1,7 +1,9 @@
 package com.digitalinnovation.personapi.service;
 
-import com.digitalinnovation.personapi.dto.MessageResponseDTO;
+import com.digitalinnovation.personapi.dto.request.PersonDTO;
+import com.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import com.digitalinnovation.personapi.entity.Person;
+import com.digitalinnovation.personapi.dto.mapper.PersonMapper;
 import com.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,13 +13,17 @@ public class PersonService {
 
     private PersonRepository personRepository;
 
+    private final PersonMapper personMApper = PersonMapper.INSTANCE;
+
     @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson(Person person){
-        Person savedPerson = personRepository.save(person);
+    public MessageResponseDTO createPerson(PersonDTO personDTO){
+        Person personToSave = personMApper.toModel(personDTO);
+
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO.builder().message("Created person with ID " + savedPerson.getId()).build();
     }
 }
