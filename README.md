a7ad91a7fe87faf9e9d22b4a62dee7e3d3e8af86
rodyokoo <nihonrodrigo@hotmail.com>
2021-08-05 22:12:55 -0400

    Add read me

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# person-api
+# person-api Digital Innovation One
+
+Criação de uma API para cadastro de pessoas e seus telefones.
+
+A implementação foi feita conforme aula de desenvolvimento API REST com Spring Boot disponibilizada na plataforma Digita Innovation One.
+
--- src/main/java/com/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/com/digitalinnovation/personapi/service/PersonService.java
@@ -6,6 +6,7 @@ import com.digitalinnovation.personapi.entity.Person;
 import com.digitalinnovation.personapi.dto.mapper.PersonMapper;
 import com.digitalinnovation.personapi.exception.PersonNotFoundException;
 import com.digitalinnovation.personapi.repository.PersonRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,16 +15,12 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
 
     private PersonRepository personRepository;
 
     private final PersonMapper personMapper = PersonMapper.INSTANCE;
-
-    @Autowired
-    public PersonService(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
 
     public MessageResponseDTO createPerson(PersonDTO personDTO){
         Person personToSave = personMapper.toModel(personDTO);
